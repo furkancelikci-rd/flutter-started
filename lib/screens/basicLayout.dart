d1c0400b37ff438da6b0245cd5a4308c5eb745a1
furkan.celikci <furkan.celikci@foriba.com>
2020-01-31 10:06:57 +0300

    box, images, gradients.

--- lib/screens/basicLayout.dart
+++ lib/screens/basicLayout.dart
@@ -13,8 +13,30 @@ class _BasicLayoutState extends State<BasicLayout> {
       appBar: AppBar(
         title: Text("Deneme Üst Başlık"),
       ),
-      body: Center(
-        child: Text("Orta Alan"),
+      body: Container(
+        margin: EdgeInsets.all(50.0),
+        width: 100.0,
+        height: 100.0,
+        // color: Colors.purple, //-decoration prop before
+        decoration: BoxDecoration(
+            color: Colors.purple,
+            gradient: LinearGradient(
+                //begin: Alignment.topCenter,
+                //end: Alignment.bottomCenter,
+                begin: Alignment(0.0, -1.0),
+                end: Alignment(0.0, 1.0),
+                colors: [Colors.orange[50], Colors.orange[500]]),
+            image: DecorationImage(
+                image: NetworkImage('http://bit.ly/flutter_tiger'),
+                fit: BoxFit.cover,
+                // repeat: ImageRepeat.repeatX
+                ),
+            // shape: BoxShape.circle
+            shape: BoxShape.rectangle,
+            //borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25.0),
+                bottomRight: Radius.circular(25.0))),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.outlined_flag),
