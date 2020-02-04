c2ac8af01f085f809879ddd8567ef36ee1170e3b
furkan.celikci <furkan.celikci@foriba.com>
2020-02-04 18:02:10 +0300

    start navigation module

---
+++ lib/screens/Navigation/art_route.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ArtRoute extends StatelessWidget {
+  final String art;
+  ArtRoute({@required this.art});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
